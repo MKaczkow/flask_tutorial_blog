cb3f2ee18b2e1acd63560e0d01228654be8c1b39
MKaczkow <maciej.kaczkowski.stud@pw.edu.pl>
2021-08-25 16:01:53 +0200

    forms work in progess

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,50 @@
 from flask import Flask, render_template
+
+from forms import RegistrationForm, LoginForm
+from secret_settings import flask_secret_key
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = flask_secret_key
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-	
+    app.run(debug=True)
